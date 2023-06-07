ec0200b3ab3c43531f3ec68fbad7f0faaf421243
Le <z5313327@ad.unsw.edu.au>
2023-06-07 19:22:59 +1000

    Added adminQuizInfo to quiz.js

---
+++ src/quiz.js
@@ -0,0 +1,15 @@
+/** 
+  * Get all of the relevant information about the current quiz.
+  * @param {number} authUserId
+  * @param {number} quizId
+  * @returns {{quizId: number, name: string, timeCreated: number, timeLastEdited: number, description: string}}
+**/
+function adminQuizInfo(authUserId, quizId) {
+    return {
+      quizId: 1,
+      name: 'My Quiz',
+      timeCreated:  1683125870,
+      timeLastEdited: 1683125871,
+      description: 'This is my quiz',  
+    }
+  }
