5de56223016904fbde1eaca7eb8543067b08c266
z5422362 <z5422362@vx12.orchestra.cse.unsw.EDU.AU>
2023-06-07 10:45:28 +1000

    adminUserDetails stub completed

---
+++ src/auth.js
@@ -0,0 +1,25 @@
+/**
+  * Given an admin user's authUserId, return details about the user
+  * 
+  * @param {number} authUserId - User's identification 
+  * 
+  * ...
+  * 
+  * @returns {object} user 
+  * 
+*/
+
+function adminUserDetails(authUserId) {
+  return {
+    user:
+    {
+      userId: 1,
+      name: 'Hayden Smith',
+      email: 'hayden.smith@unsw.edu.au',
+      numSuccessfulLogins: 3,
+      numFailedPasswordsSinceLastLogin: 1,
+    }
+  };
+}
+
+
