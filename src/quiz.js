aa4cb2dd8c6d54c716ebf8e69c9948f98900179d
Le <z5313327@ad.unsw.edu.au>
2023-06-07 19:28:48 +1000

    Added adminQuizNameUpdate to quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -1,13 +1,14 @@
 /**
- * Update name of relevant quiz.
- * @param {number} authUserId 
- * @param {number} quizId 
- * @param {string} name 
- * @returns {{}}
+  * Update name of relevant quiz.
+  * @param {number} authUserId 
+  * @param {number} quizId 
+  * @param {string} name 
+  * @returns {{}}
 **/
 function adminQuizNameUpdate(authUserId, quizId, name) {
 	return {
-
+  
 	}
-}
+  }
+  
   
