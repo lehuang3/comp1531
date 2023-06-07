f71378188c9246d306a0581392041fa9f272d3b8
Le <z5313327@ad.unsw.edu.au>
2023-06-07 21:10:04 +1000

    Added adminQuizNameUpdate to quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -1,3 +1,4 @@
+
 /**
   * Update name of relevant quiz.
   * @param {number} authUserId 
@@ -9,6 +10,37 @@ function adminQuizNameUpdate(authUserId, quizId, name) {
 	return {
   
 	}
+}
+  
+/**
+ * Provide a list of all quizzes that are owned by the currently logged in user.
+ * @param {integer} authUserId - Admin user ID.
+ * @returns {array object} - List of quizzes.
+**/
+function adminQuizList(authUserId) {
+  return {
+    quizzes: [
+      {
+        quizId: 1,
+        name: 'My Quiz',
+      }
+    ]
+	}
+}
+
+/**
+  * Update the description of the relevant quiz.
+  * 
+  * @param {number} authUserId - User's identification number
+  * @param {number} quizId - Quiz's identification number
+  * @param {string} description - Quiz's description
+  * ...
+  * 
+  * @returns {empty}
+**/
+function adminQuizDescriptionUpdate(authUserId, quizId, description) {
+  return {
+
   }
-  
-  
+}
+
