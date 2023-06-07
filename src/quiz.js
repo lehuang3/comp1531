1237fb894ae06253a0d3294cac5766e6b8081557
Le <z5313327@ad.unsw.edu.au>
2023-06-07 19:18:54 +1000

    Added adminQuizNameUpdate to quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,13 @@
-/** 
-  * Get all of the relevant information about the current quiz.
-  * @param {number} authUserId
-  * @param {number} quizId
-  * @returns {{quizId: number, name: string, timeCreated: number, timeLastEdited: number, description: string}}
+/**
+ * Update name of relevant quiz.
+ * @param {number} authUserId 
+ * @param {number} quizId 
+ * @param {string} name 
+ * @returns {{}}
 **/
-function adminQuizInfo(authUserId, quizId) {
-    return {
-      quizId: 1,
-      name: 'My Quiz',
-      timeCreated:  1683125870,
-      timeLastEdited: 1683125871,
-      description: 'This is my quiz',  
-    }
-  }
+function adminQuizNameUpdate(authUserId, quizId, name) {
+	return {
+
+	}
+}
+  
