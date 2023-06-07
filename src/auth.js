49a7277736bd2f474325d34d7485c3ce79266fd5
Pranav Patel <z5418158@ad.unsw.edu.au>
2023-06-07 19:25:35 +1000

    Added stub code for adminAuthRegister

---
+++ src/auth.js
@@ -0,0 +1,16 @@
+/**
+ * @returns {{authUserId: number}} object
+ */
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+    // Return authUserId: 1 if user successfully registers or 0 if unsuccessful
+    //if (something) {
+      return {
+             authUserId: 1,
+      } 
+    /*} else (something) {
+          return {
+              authUserId: 0,
+          } 
+    }*/ 
+}
+
