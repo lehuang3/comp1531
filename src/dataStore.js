1c62fe4f3b2a1dd2ea85e0521f33c7882c148b50
Sina <z5482123@ad.unsw.edu.au>
2023-06-13 19:31:44 +1000

    fix: Added a userQuiz array which keeps track of specific user arrays

--- src/dataStore.js
+++ src/dataStore.js
@@ -13,6 +13,42 @@ let data = {
 // YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
 
 /*
+
+let data = {
+    
+  // User Data
+  users: [
+    {
+    UserId: 1,
+    nameFirst: 'joe',
+    nameLast: 'devon',
+    email: 'joe.devon@gmail.com',
+    password: "test123",
+    numSuccessfulLogins: 3,
+    numFailedPasswordsSinceLastLogin: 1,
+    userQuizs:[1]
+    }
+  ],
+    
+
+  //Quiz Data
+  quizzes: [
+    {
+      quizId: 1,
+      name: 'My Quiz',
+      timeCreated: 1683125870,
+      timeLastEdited: 1683125871,
+      description: 'This is my quiz',
+    }
+  ]
+	
+}
+
+
+*/
+
+
+/*
 Example usage
     let store = getData()
     console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Rando'] }
