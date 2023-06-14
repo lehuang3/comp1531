9b350a454a9760e76662c853a5962a9671c60239
Le <z5313327@ad.unsw.edu.au>
2023-06-14 18:42:23 +1000

    Added base tests, testing for each invalid inputs and valid input

---
+++ src/adminQuizInfo.test.js
@@ -0,0 +1,23 @@
+import { adminQuizInfo } from './quiz.js'
+
+beforeEach(() => {
+  clear();
+})
+
+test('Test with valid inputs', () => {
+  
+})
+
+
+test('Test with invalid authUserId', () => {
+
+})
+
+
+test('Test with invalid quiz id', () => {
+
+})
+
+test('Test with user not having ownership of quiz', () => {
+
+})
