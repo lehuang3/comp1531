9784bebaf5a29d802703dc75239b45b35e9f1fe4
Le <z5313327@ad.unsw.edu.au>
2023-06-14 18:32:18 +1000

    Created quizValidCheck.test.js

---
+++ src/quizValidCheck.test.js
@@ -0,0 +1,14 @@
+import { quizValidCheck } from './quizValidCheck.js';
+import { clear } from './other.js';
+import { getData, setData } from 'dataStore.js';
+
+beforeEach(() => {
+  clear();
+})
+
+test('Test when quiz is valid', () => {
+  let data = getData();
+  setData
+
+
+});
