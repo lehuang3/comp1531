dbb329e7396a0353707dc32e0f7973613fc0f9fe
Le <z5313327@ad.unsw.edu.au>
2023-06-16 15:33:10 +1000

    Exported function from quiz.js and added code to adminQuizInfo.test.js

--- src/adminQuizInfo.test.js
+++ src/adminQuizInfo.test.js
@@ -1,23 +1,25 @@
-import { adminQuizInfo } from './quiz.js'
+import { adminQuizInfo } from './quiz.js';
+import { clear } from './other.js';
 
-beforeEach(() => {
-  clear();
-})
+// beforeEach(() => {
+//   clear();
+// })
 
 test('Test with valid inputs', () => {
   
-})
+  
+});
 
 
 test('Test with invalid authUserId', () => {
-
-})
+  
+});
 
 
 test('Test with invalid quiz id', () => {
-
-})
+  
+});
 
 test('Test with user not having ownership of quiz', () => {
-
-})
+  
+});
--- src/quiz.js
+++ src/quiz.js
@@ -105,3 +105,5 @@ function adminQuizDescriptionUpdate(authUserId, quizId, description) {
 
   }
 }
+
+export { adminQuizInfo }
