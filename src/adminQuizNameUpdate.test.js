685d8914df5dbc800467833119b7f673cc669472
Le <z5313327@ad.unsw.edu.au>
2023-06-16 18:36:16 +1000

    Added basic tests in adminQuizNameUpdate.test.js and exported relevant functions from relevant files

--- src/adminQuizNameUpdate.test.js
+++ src/adminQuizNameUpdate.test.js
@@ -1,57 +1,58 @@
-import { adminQuizNameUpdate } from './quiz.js'
+import { adminQuizNameUpdate, adminQuizCreate } from './quiz.js'
+import { adminAuthLogin, adminAuthRegister } from './auth.js'
 
 test ('simple test pass', () => {
-  adminAuthRegister();
-  adminAuthLogin();
-  adminQuizCreate();
+  adminAuthRegister('123@email.com', '123', 'david', 'test');
+  let user = adminAuthLogin('123@email.com', '123')
+  let quiz = adminQuizCreate(user.authUserId, 'quiz', 'quiz1');
   
-  expect(adminQuizNameUpdate()).toStrictEqual()
+  expect(adminQuizNameUpdate(user.authUserId, quiz.quizId, 'quiz2')).toStrictEqual({ })
 }) 
 
 test ('userId not valid', () => {
-  adminAuthRegister();
-  adminAuthLogin();
-  adminQuizCreate();
+  adminAuthRegister('123@email.com', '123', 'david', 'test');
+  let user = adminAuthLogin('123@email.com', '123')
+  let quiz = adminQuizCreate(user.authUserId, 'quiz', 'quiz1');
+  let user2 = adminAuthRegister('1234@email.com', '123', 'jack', 'test');
   
-  expect(adminQuizNameUpdate()).toStrictEqual({ error: ''})
+  expect(adminQuizNameUpdate(user2.authUserId, quiz.quizId, 'quiz2')).toStrictEqual({ error: 'You do not have access to this quiz.'})
 }) 
 
 test ('quizId not valid', () => {
-  adminAuthRegister();
-  adminAuthLogin();
-  adminQuizCreate();
-  
-  expect(adminQuizNameUpdate()).toStrictEqual({ error: 'Quiz does not exist.'})
+  adminAuthRegister('123@email.com', '123', 'david', 'test');
+  let user = adminAuthLogin('123@email.com', '123')
+  let quiz = adminQuizCreate(user.authUserId, 'quiz', 'quiz1');
+  let user2 = adminAuthRegister('1234@email.com', '123', 'jack', 'test');
+  adminAuthLogin('123@email.com', '123')
+  let quiz2 = adminQuizCreate(user2.authUserId, 'quiz', 'quiz1');
+
+  expect(adminQuizNameUpdate(user2.authUserId, quiz2.quizId, 'quiz2')).toStrictEqual({ error: 'Quiz does not exist.'})
 }) 
 
 test ('quiz name not valid', () => {
-  adminAuthRegister();
-  adminAuthLogin();
-  adminQuizCreate();
+  adminAuthRegister('123@email.com', '123', 'david', 'test');
+  let user = adminAuthLogin('123@email.com', '123')
+  let quiz = adminQuizCreate(user.authUserId, 'quiz', 'quiz1');
   
-  expect(adminQuizNameUpdate()).toStrictEqual({ error: 'Quiz name cannot have spaces and special characers.'})
+  expect(adminQuizNameUpdate(user.authUserId, quiz.quizId, 'quiz#')).toStrictEqual({ error: 'Quiz name cannot have spaces and special characters.'})
+  expect(adminQuizNameUpdate(user.authUserId, quiz.quizId, 'quiz 1')).toStrictEqual({ error: 'Quiz name cannot have spaces and special characters.'})
 }) 
 
-test ('quiz name too short', () => {
-  adminAuthRegister();
-  adminAuthLogin();
-  adminQuizCreate();
+test ('quiz name too long or short', () => {
+  adminAuthRegister('123@email.com', '123', 'david', 'test');
+  let user = adminAuthLogin('123@email.com', '123')
+  let quiz = adminQuizCreate(user.authUserId, 'quiz', 'quiz1');
   
-  expect(adminQuizNameUpdate()).toStrictEqual({ error: 'Quiz name must be greater or equal to 2 chartacters and less than or equal to 30.'})
+  expect(adminQuizNameUpdate(user.authUserId, quiz.quizId, 'q1')).toStrictEqual({ error: 'Quiz name must be greater or equal to 2 chartacters and less than or equal to 30.'})
+  expect(adminQuizNameUpdate(user.authUserId, quiz.quizId, 'qdhjfasjkhfjasjhfkasghjdhfjasbgasdg')).toStrictEqual({ error: 'Quiz name must be greater or equal to 2 chartacters and less than or equal to 30.'})
 }) 
 
-test ('quiz name too long', () => {
-  adminAuthRegister();
-  adminAuthLogin();
-  adminQuizCreate();
-  
-  expect(adminQuizNameUpdate()).toStrictEqual({ error: 'Quiz name must be greater or equal to 2 chartacters and less than or equal to 30.'})
-}) 
 
 test ('quiz name already used', () => {
-  adminAuthRegister();
-  adminAuthLogin();
-  adminQuizCreate();
+  adminAuthRegister('123@email.com', '123', 'david', 'test');
+  let user = adminAuthLogin('123@email.com', '123')
+  let quiz = adminQuizCreate(user.authUserId, 'quiz', 'quiz1');
+  adminQuizCreate(user.authUserId, 'quiz', 'quiz2');
   
-  expect(adminQuizNameUpdate()).toStrictEqual({ error: 'Quiz name already exists.'})
+  expect(adminQuizNameUpdate(user.authUserId, quiz.quizId, 'quiz2')).toStrictEqual({ error: 'Quiz name already exists.'})
 }) 
