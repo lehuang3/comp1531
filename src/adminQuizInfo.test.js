60b3a7ea35667e42785219b7eb66bf63635e071b
Le <z5313327@ad.unsw.edu.au>
2023-06-16 17:18:17 +1000

    Implementing tests for adminQuizInfo

--- src/adminQuizInfo.test.js
+++ src/adminQuizInfo.test.js
@@ -1,25 +1,78 @@
 import { adminQuizInfo } from './quiz.js';
 import { clear } from './other.js';
+import { getData, setData} from './dataStore.js'
 
 // beforeEach(() => {
 //   clear();
 // })
 
-test('Test with valid inputs', () => {
+test ('simple test', () => {
+  let data = getData;
+  data.users.push({
+    UserId: 1,
+    nameFirst: 'joe',
+    nameLast: 'devon',
+    email: 'joe.devon@gmail.com',
+    password: "test123",
+    numSuccessfulLogins: 3,
+    numFailedPasswordsSinceLastLogin: 1,
+    userQuizs:[1]
+  })
+  data.quizzes.push({
+    quizId: 1,
+    name: quiz,
+    timeCreated: 1,
+    timeLastEditied: 1,
+    description: 'simple quiz',
+  })
+  expect(adminQuizInfo(1,1)).toStrictEqual({
+    quizId: 1,
+    name: quiz,
+    timeCreated: 1,
+    timeLastEditied: 1,
+    description: 'simple quiz',
+  })
+}
+
+)
+
+test ('simple test', () => {
+  adminAuthRegister('z123@edu.com', '123', 'test', 'test');
+  adminAuthLogin('z123@edu.com', '123');
+  adminQuizCreate('')
   
-  
-});
+})
 
 
-test('Test with invalid authUserId', () => {
-  
-});
 
+// describe('Tests with valid inputs', () => {
+//   //clear();
+//   let data = getData();
+//   test.each(
 
-test('Test with invalid quiz id', () => {
-  
-});
+//   )
+// });
 
-test('Test with user not having ownership of quiz', () => {
-  
-});
+// describe('Tests with invalid authUserId', () => {
+//   //clear();
+//   let data = getData();
+//   test.each(
+
+//   )
+// });
+
+// describe('Test with invalid quiz id', () => {
+//   //clear();
+//   let data = getData();
+//   test.each(
+
+//   )
+// });
+
+// describe('Test with user not having ownership of quiz', () => {
+//   //clear();
+//   let data = getData();
+//   test.each(
+
+//   )
+// });
