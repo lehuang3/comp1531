67653433031d08921629637e80560835b315bd09
Pranav Patel <z5418158@ad.unsw.edu.au>
2023-06-18 20:32:16 +1000

    Bugfix - Minor fix to clear tests

--- src/clear.test.js
+++ src/clear.test.js
@@ -1,23 +1,22 @@
 import { adminAuthRegister, adminAuthLogin } from './auth.js'
-import { getData } from './dataStore.js';
 import { clear } from './other.js'
 
 // Test if the clear function is clearing the data values i.e. if we call adminAuthLogin then clear, we should be able to login again as a different user.
 describe('Testing clear is resetting database', () => {
-    test('Clear database test 1', () => {
+    beforeEach(() => {
         adminAuthRegister('santaclaus@gmail.com', 'S@nta23!', 'Santa', 'Claus');
         clear();
+    });
+    
+    test('Clear database test 1', () => {
         let result = adminAuthRegister('santaclaus@gmail.com', 'S@nta23!', 'Santa', 'Claus');
         expect(result).toEqual(expect.any(Number));
     });
     
     test('Clear database test 2', () => {
-        adminAuthRegister('santaclaus@gmail.com', 'S@nta23!', 'Santa', 'Claus');
         adminAuthRegister('patel@gmail.com', 'Abcd123!', 'Pranav', 'Patel');
-        adminAuthLogin('patel@gmail.com', 'Abcd123!');
-        clear();
-        let result = getData();
-        expect(result).toStrictEqual({});
+        let result = adminAuthLogin('santaclaus@gmail.com', 'S@nta23!');
+        expect(result).toStrictEqual({error: 'email is invalid'});
     });
 });
 
