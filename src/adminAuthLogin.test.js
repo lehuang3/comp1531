f5ca5cb8c3f50b73eafa307cfb27dd3151a17599
Pranav Patel <z5418158@ad.unsw.edu.au>
2023-06-18 18:57:19 +1000

    Bugfix -> Modified/improved tests for adminAuthLogin

--- src/adminAuthLogin.test.js
+++ src/adminAuthLogin.test.js
@@ -2,25 +2,30 @@ import { adminAuthRegister, adminAuthLogin } from './auth.js'
 import { clear } from './other.js'
 
 // Tests to make sure that adminAuthLogin is working correctly when it should and returning errors when it should
+describe('tests for adminAuthLogin', () => {
+	beforeEach(() => {
+		clear();
+		adminAuthRegister('patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel');
+	});
 
-beforeEach(() => {
-	clear();
-});
+	describe('adminAuthLogin success tests', () => {
+		test('Simple test pass', () => {
+			let userLogin = adminAuthLogin('patel@gmail.com', 'Abcd123%');
+			expect(userLogin).toStrictEqual(expect.any(Number));
+		});
+	});
 
-test('Simple test pass', () => {
-	adminAuthRegister('patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel');
-	let userLogin = adminAuthLogin('patel@gmail.com', 'Abcd123%');
-	expect(userLogin).toStrictEqual(users.UserID);
-});
+	describe('adminAuthLogin invalid email', () => {
+		test('Email invalid', () => {
+			let userLogin = adminAuthLogin('patel2@gmail.com', 'Abcd123%');
+			expect(userLogin).toStrictEqual({ error: 'error: email address is does not exist' });
+		});
+	});
 
-test('Email invalid', () => {
-	adminAuthRegister('patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel');
-	let userLogin = adminAuthLogin('patel2@gmail.com', 'Abcd123%');
-	expect(userLogin).toStrictEqual('error: email address is does not exist');
-});
-
-test('Password incorrect', () => {
-	adminAuthRegister('patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel');
-	let userLogin = adminAuthLogin('patel@gmail.com', 'abcd123%');
-	expect(userLogin).toStrictEqual('error: password incorrect');
+	describe('adminAuthLogin invalid password', () => {
+		test('Password incorrect', () => {
+			let userLogin = adminAuthLogin('patel@gmail.com', 'abcd123%');
+			expect(userLogin).toStrictEqual({ error: 'error: password incorrect' });
+		});
+	});
 });
