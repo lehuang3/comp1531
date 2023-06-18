5ff65d248a463a740fea5866a7d283d633ad7a0f
Pranav Patel <z5418158@ad.unsw.edu.au>
2023-06-18 18:06:21 +1000

    Bugfixes -> Modified tests for clear

--- src/clear.test.js
+++ src/clear.test.js
@@ -1,13 +1,28 @@
 import { adminAuthRegister, adminAuthLogin } from './auth.js'
+import { getData } from './dataStore.js';
 import { clear } from './other.js'
 
 // Test if the clear function is clearing the data values i.e. if we call adminAuthLogin then clear, we should be able to login again as a different user.
+describe('Testing clear is resetting database', () => {
+    test('Clear database test 1', () => {
+        adminAuthRegister('santaclaus@gmail.com', 'S@nta23!', 'Santa', 'Claus');
+        clear();
+        let result = adminAuthRegister('santaclaus@gmail.com', 'S@nta23!', 'Santa', 'Claus');
+        expect(result).toEqual(expect.any(Number));
+    });
+    
+    test('Clear database test 2', () => {
+        adminAuthRegister('santaclaus@gmail.com', 'S@nta23!', 'Santa', 'Claus');
+        adminAuthRegister('patel@gmail.com', 'Abcd123!', 'Pranav', 'Patel');
+        adminAuthLogin('patel@gmail.com', 'Abcd123!');
+        clear();
+        let result = getData();
+        expect(result).toStrictEqual({});
+    });
+});
 
-test('Test successful for clear', () => {
-	adminAuthRegister('santaclaus@gmail.com', 'S@nta23!', 'Santa', 'Claus');
-	adminAuthRegister('patel@gmail.com', 'Abcd123!', 'Pranav', 'Patel');
-	adminAuthLogin('patel@gmail.com', 'Abcd123!');
-	clear();
-	let result = adminAuthLogin('santaclaus@gmail.com', 'S@nta23!');
-	expect(result).toBe(users.UserID);
+describe('Testing clear return value', () => {
+    test('Clear return value test', () => {        
+        expect(clear()).toStrictEqual({});
+    });
 });
