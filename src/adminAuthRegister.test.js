92e211e60b85c0b925bd7e2d8b432c523454eaf6
Sina <z5482123@ad.unsw.edu.au>
2023-06-21 09:48:08 +1000

    Styling: Added eslint and fixed code styling

--- src/adminAuthRegister.test.js
+++ src/adminAuthRegister.test.js
@@ -3,69 +3,69 @@ import { clear } from './other.js'
 
 // Test if the adminAuthRegister function is returning errors correctly and passing when it should.
 describe('adminAuthRegister tests', () => {
-	beforeEach(() => {
-		clear();
-	});
+  beforeEach(() => {
+    clear()
+  })
 
-	describe('Testing valid registrations', () => {
-		test('Simple test pass', () => {
-			let user = adminAuthRegister('patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel');
-			expect(user).toStrictEqual({authUserId: expect.any(Number)});
-		});
+  describe('Testing valid registrations', () => {
+    test('Simple test pass', () => {
+      const user = adminAuthRegister('patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel')
+      expect(user).toStrictEqual({ authUserId: expect.any(Number) })
+    })
 
-		test('Name assumption pass', () => {
-			let user = adminAuthRegister('patel@gmail.com', 'Abcd123%', '       ', '-------');
-			expect(user).toStrictEqual({authUserId: expect.any(Number)});
-		});
-	});
+    test('Name assumption pass', () => {
+      const user = adminAuthRegister('patel@gmail.com', 'Abcd123%', '       ', '-------')
+      expect(user).toStrictEqual({ authUserId: expect.any(Number) })
+    })
+  })
 
-	describe('Testing invalid emails', () => {
-		test('Email is used by another user', () => {
-			let user = adminAuthRegister('patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel');
-            expect(user).toStrictEqual({authUserId: expect.any(Number)});
-			let invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'Santa', 'Claus');
-			expect(invalidUser).toStrictEqual({ error: 'error: email is already used for another account' });
-		});
+  describe('Testing invalid emails', () => {
+    test('Email is used by another user', () => {
+      const user = adminAuthRegister('patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel')
+      expect(user).toStrictEqual({ authUserId: expect.any(Number) })
+      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'Santa', 'Claus')
+      expect(invalidUser).toStrictEqual({ error: 'error: email is already used for another account' })
+    })
 
-		test('Email is not valid', () => {
-			let invalidUser = adminAuthRegister('patel@@gmailcom', 'Vdhr347@', 'Santa', 'Claus');
-			expect(invalidUser).toStrictEqual({ error: 'error: email is not valid' });
-		});
-	});
+    test('Email is not valid', () => {
+      const invalidUser = adminAuthRegister('patel@@gmailcom', 'Vdhr347@', 'Santa', 'Claus')
+      expect(invalidUser).toStrictEqual({ error: 'error: email is not valid' })
+    })
+  })
 
-	describe('Testing invalid first names', () => {
-		test('First name contains invalid characters', () => {
-			let invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', '$@nta', 'Claus');
-			expect(invalidUser).toStrictEqual({ error: 'error: first name contains invalid characters' });
-		});
+  describe('Testing invalid first names', () => {
+    test('First name contains invalid characters', () => {
+      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', '$@nta', 'Claus')
+      expect(invalidUser).toStrictEqual({ error: 'error: first name contains invalid characters' })
+    })
 
-		test('First name has invalid length', () => {
-			let invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'S', 'Claus');
-			expect(invalidUser).toStrictEqual({ error: 'error: first name has an invalid length' });
-		});
-	});
+    test('First name has invalid length', () => {
+      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'S', 'Claus')
+      expect(invalidUser).toStrictEqual({ error: 'error: first name has an invalid length' })
+    })
+  })
 
-	describe('Testing invalid last names', () => {
-		test('Last name contains invalid characters', () => {
-			let invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'Santa', 'C!aus');
-			expect(invalidUser).toStrictEqual({ error: 'error: last name contains invalid characters' });
-		});
+  describe('Testing invalid last names', () => {
+    test('Last name contains invalid characters', () => {
+      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'Santa', 'C!aus')
+      expect(invalidUser).toStrictEqual({ error: 'error: last name contains invalid characters' })
+    })
 
-		test('Last name has invalid length', () => {
-			let invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'Santa', 'Clausandhislittlehelperfriends');
-			expect(invalidUser).toStrictEqual({ error: 'error: last name has an invalid length' });
-		});
-	});
+    test('Last name has invalid length', () => {
+      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'Santa', 'Clausandhislittlehelperfriends')
+      expect(invalidUser).toStrictEqual({ error: 'error: last name has an invalid length' })
+    })
+  })
 
-	describe('Testing invalid passwords', () => {
-		test('Short password test', () => {
-			let invalidUser = adminAuthRegister('patel@gmail.com', 'Ys23&!', 'Santa', 'Claus');
-			expect(invalidUser).toStrictEqual({ error: 'error: password is too short' });
-		});
+  describe('Testing invalid passwords', () => {
+    test('Short password test', () => {
+      const invalidUser = adminAuthRegister('patel@gmail.com', 'Ys23&!', 'Santa', 'Claus')
+      expect(invalidUser).toStrictEqual({ error: 'error: password is too short' })
+    })
 
-		test('Weak password test', () => {
-			let invalidUser = adminAuthRegister('patel@gmail.com', 'vdhr@!&hds', 'Santa', 'Claus');
-			expect(invalidUser).toStrictEqual({ error: 'error: password is too weak' });
-		});
-	});
-});
+    test('Weak password test', () => {
+      const invalidUser = adminAuthRegister('patel@gmail.com', 'vdhr@!&hds', 'Santa', 'Claus')
+      expect(invalidUser).toStrictEqual({ error: 'error: password is too weak' })
+    })
+  })
+})
--- src/adminQuizRemove.test.js
+++ src/adminQuizRemove.test.js
@@ -1,58 +1,55 @@
-import { adminQuizCreate,adminQuizRemove , adminQuizList} from './quiz.js';
-import { adminAuthRegister } from './auth.js';
-import { clear } from './other.js';
+import { adminQuizCreate, adminQuizRemove, adminQuizList } from './quiz.js'
+import { adminAuthRegister } from './auth.js'
+import { clear } from './other.js'
 
 // Runs before each test
 beforeEach(() => {
-  clear();
-});
+  clear()
+})
 
 test('Invalide User ID', () => {
-  adminAuthRegister("Sina.hafezimasoomi@gmail.com", "Sina12356789", "Sina", "Hafezi");
-  adminQuizCreate(0, 'Sina',"descruiption");
-  expect(adminQuizRemove(1,0)).toStrictEqual({ error: expect.any(String) });
-});
+  adminAuthRegister('Sina.hafezimasoomi@gmail.com', 'Sina12356789', 'Sina', 'Hafezi')
+  adminQuizCreate(0, 'Sina', 'descruiption')
+  expect(adminQuizRemove(1, 0)).toStrictEqual({ error: expect.any(String) })
+})
 
 test('Invalide quiz ID', () => {
-  adminAuthRegister("Sina.hafezimasoomi@gmail.com", "Sina12356789", "Sina", "Hafezi");
-  adminAuthRegister("hayden.hafezimasoomi@gmail.com", "hayden", "hayden", "Hafezi");
-  adminQuizCreate(0, 'Sina',"descruiption");
-  adminQuizCreate(1, 'hayden',"descruiption");
-  expect(adminQuizRemove(1,0)).toStrictEqual({ error: expect.any(String) });
-});
-
+  adminAuthRegister('Sina.hafezimasoomi@gmail.com', 'Sina12356789', 'Sina', 'Hafezi')
+  adminAuthRegister('hayden.hafezimasoomi@gmail.com', 'hayden', 'hayden', 'Hafezi')
+  adminQuizCreate(0, 'Sina', 'descruiption')
+  adminQuizCreate(1, 'hayden', 'descruiption')
+  expect(adminQuizRemove(1, 0)).toStrictEqual({ error: expect.any(String) })
+})
 
 test('Invalide User ID', () => {
-  adminAuthRegister("Sina.hafezimasoomi@gmail.com", "Sina12356789", "Sina", "Hafezi");
-  adminQuizCreate(0, 'Sina',"descruiption");
-  expect(adminQuizRemove(1,0)).toStrictEqual({ error: expect.any(String) });
-});
+  adminAuthRegister('Sina.hafezimasoomi@gmail.com', 'Sina12356789', 'Sina', 'Hafezi')
+  adminQuizCreate(0, 'Sina', 'descruiption')
+  expect(adminQuizRemove(1, 0)).toStrictEqual({ error: expect.any(String) })
+})
 
 test('Valid entry', () => {
-	adminAuthRegister("Sina.hafezimasoomi@gmail.com", "Sina12356789", "Sina", "Hafezi");
-  adminQuizCreate(0, 'Sina',"descruiption");
-	adminQuizCreate(0, 'Sina1',"descruiption");
-  expect(adminQuizList(0)).toStrictEqual({ 
-		quizzes: [
-			{
-				quizId: expect.any(Number),
-				name: expect.any(String),
-			},
-			{
-				quizId: expect.any(Number),
-				name: expect.any(String),
-			}
+  adminAuthRegister('Sina.hafezimasoomi@gmail.com', 'Sina12356789', 'Sina', 'Hafezi')
+  adminQuizCreate(0, 'Sina', 'descruiption')
+  adminQuizCreate(0, 'Sina1', 'descruiption')
+  expect(adminQuizList(0)).toStrictEqual({
+    quizzes: [
+      {
+        quizId: expect.any(Number),
+        name: expect.any(String)
+      },
+      {
+        quizId: expect.any(Number),
+        name: expect.any(String)
+      }
   	]
-	});
-
-});
+  })
+})
 
 test('Removed in user datastore', () => {
-	
-  adminAuthRegister("Sina.hafezimasoomi@gmail.com", "Sina12356789", "Sina", "Hafezi");
-  adminQuizCreate(0, 'quiz1',"descruiption");
-  adminQuizCreate(0, 'quiz2',"descruiption");
-  adminQuizCreate(0, 'quiz3',"descruiption");
-  adminQuizRemove(0,1);
-  expect(adminQuizRemove(0,0)).toStrictEqual({});
-});
+  adminAuthRegister('Sina.hafezimasoomi@gmail.com', 'Sina12356789', 'Sina', 'Hafezi')
+  adminQuizCreate(0, 'quiz1', 'descruiption')
+  adminQuizCreate(0, 'quiz2', 'descruiption')
+  adminQuizCreate(0, 'quiz3', 'descruiption')
+  adminQuizRemove(0, 1)
+  expect(adminQuizRemove(0, 0)).toStrictEqual({})
+})
