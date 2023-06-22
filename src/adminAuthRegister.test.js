369df1b21c32061893ed7ed389ab2ba388a9289d
Pranav Patel <z5418158@ad.unsw.edu.au>
2023-06-22 21:59:03 +1000

    Testing -> new tests added to adminAuthRegister

--- src/adminAuthRegister.test.js
+++ src/adminAuthRegister.test.js
@@ -20,46 +20,80 @@ describe('adminAuthRegister tests', () => {
   })
 
   describe('Testing invalid emails', () => {
-    test('Email is used by another user', () => {
-      const user = adminAuthRegister('patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel')
+    test.each([
+        ['patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel'],
+        ['patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel'],
+        ['patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel'],
+        ['patel@gmail.com', 'Abcd123%', 'Pranav', 'Patel'],
+    ])('Email is used by another user, testing (%s, %s, %s, %s)', (email, password, nameFirst, nameLast) => {
+      const user = adminAuthRegister(email, password, nameFirst, nameLast)
       expect(user).toStrictEqual({ authUserId: expect.any(Number) })
-      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'Santa', 'Claus')
+      const invalidUser = adminAuthRegister(email, password, nameFirst, nameLast)
       expect(invalidUser).toStrictEqual({ error: 'error: email is already used for another account' })
     })
 
-    test('Email is not valid', () => {
-      const invalidUser = adminAuthRegister('patel@@gmailcom', 'Vdhr347@', 'Santa', 'Claus')
+    test.each([
+        ['patel@@gmail.com'],
+        ['ashdaksjdhbmne23sakdasbmail.com'],
+        ['sussybaka@amongus'],
+        ['randomtester.net.au'],
+    ])('Email is not valid, testing: %s', (email) => {
+      const invalidUser = adminAuthRegister(email, 'Vdhr347@', 'Santa', 'Claus')
       expect(invalidUser).toStrictEqual({ error: 'error: email is not valid' })
     })
   })
 
   describe('Testing invalid first names', () => {
-    test('First name contains invalid characters', () => {
-      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', '$@nta', 'Claus')
+    test.each([
+        ['$@nta'],
+        ['ash83984'],
+        ['sussybaka@amongus'],
+        ['Pksd&^2.'],
+    ])('First name contains invalid characters, testing: %s', (nameFirst) => {
+      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', nameFirst, 'Claus')
       expect(invalidUser).toStrictEqual({ error: 'error: first name contains invalid characters' })
     })
 
-    test('First name has invalid length', () => {
-      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'S', 'Claus')
+    test.each([
+        ['L'],
+        ['ashdaksjdhbmneeeeeeeeeeeeeeesakdasbmaileqwwwwwwweecom'],
+        ['sussybakaamongusuuuuu'],
+        ['P'],
+    ])('First name has invalid length, testing: %s', (nameFirst) => {
+      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', nameFirst, 'Claus')
       expect(invalidUser).toStrictEqual({ error: 'error: first name has an invalid length' })
     })
   })
 
   describe('Testing invalid last names', () => {
-    test('Last name contains invalid characters', () => {
-      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'Santa', 'C!aus')
+    test.each([
+        ['$@nta'],
+        ['ashda123123'],
+        ['sussybaka@amongus'],
+        ['Pksd&^2.'],
+    ])('Last name contains invalid characters', (nameLast) => {
+      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'Santa', nameLast)
       expect(invalidUser).toStrictEqual({ error: 'error: last name contains invalid characters' })
     })
 
-    test('Last name has invalid length', () => {
-      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'Santa', 'Clausandhislittlehelperfriends')
+    test.each([
+        ['L'],
+        ['ashdaksjdhbmneeeeeeeeeeeeeeesakdasbmaileqwwwwwwweecom'],
+        ['sussybakaamongusuuuuu'],
+        ['P'],
+    ])('Last name has invalid length, testing: %s', (nameLast) => {
+      const invalidUser = adminAuthRegister('patel@gmail.com', 'Vdhr347@', 'Santa', nameLast)
       expect(invalidUser).toStrictEqual({ error: 'error: last name has an invalid length' })
     })
   })
 
   describe('Testing invalid passwords', () => {
-    test('Short password test', () => {
-      const invalidUser = adminAuthRegister('patel@gmail.com', 'Ys23&!', 'Santa', 'Claus')
+    test.each([
+        ['Ldasjh'],
+        ['sWe@y7'],
+        ['Pu@19'],
+    ])('Short password test, testing: %s ', (password) => {
+      const invalidUser = adminAuthRegister('patel@gmail.com', password, 'Santa', 'Claus')
       expect(invalidUser).toStrictEqual({ error: 'error: password is too short' })
     })
 
