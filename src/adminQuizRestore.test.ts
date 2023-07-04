e1f4e35b945563624a6bea3e3f85591addf2ff02
Le <z5313327@ad.unsw.edu.au>
2023-07-04 15:58:24 +1000

    Simple tests for adminQuizRestore has been added

--- src/adminQuizRestore.test.ts
+++ src/adminQuizRestore.test.ts
@@ -1,3 +1,64 @@
 import { adminQuizNameUpdate, adminQuizCreate } from './quiz.js'
 import { adminAuthLogin, adminAuthRegister } from './auth.js'
-import { requestClear, requestGetAdminUserDetails, requestAdminAuthRegister, requestAdminQuizNameUpdate, requestAdminQuizCreate} from './other'
+import { requestClear, requestGetAdminUserDetails, requestAdminAuthRegister, requestAdminQuizNameUpdate, requestAdminQuizCreate, requestAdminQuizRemove} from './other'
+
+
+let token1: any;
+let quiz1: any;
+let quiz2: any;
+let token2: any;
+let quiz3: any;
+let quiz4: any;
+
+beforeEach(() => {
+  requestClear()
+  token1 = requestAdminAuthRegister('123@email.com', '123adjakjfhgaA', 'david', 'test')
+  quiz1 = requestAdminQuizCreate(token1.body, 'quizhello', 'quiz1number')
+  quiz2 = requestAdminQuizCreate(token1.body, 'quiz123', 'quizname')
+  requestAdminQuizRemove(token1.body, quiz1.body.quizId);
+  token2 = requestAdminAuthRegister('12345@email.com', '123adjakjfhgaA', 'david', 'test')
+  quiz3 = requestAdminQuizCreate(token2.body, 'quizname1', 'quiz123')
+  quiz4 = requestAdminQuizCreate(token2.body, 'quizname', 'quiz456')
+  requestAdminQuizRemove(token2.body, quiz3.body.quizId);
+})
+
+
+describe('Passing cases', () => {
+  test('negative quizId', () => {
+    expect(requestAdminQuizRestore(token1.body, quiz1.body.quizId).body).toStrictEqual({ });
+  })
+})
+
+describe('QuizId is not valid', () => {
+  test('negative quizId', () => {
+    expect(requestAdminQuizRestore(token1.body, -1).body).toStrictEqual({ error: 'Quiz does not exist.'})
+  })
+})
+
+describe('No ownership of quiz', () => {
+  test('negative quizId', () => {
+    expect(requestAdminQuizRestore(token1.body, -1).body).toStrictEqual({ error: 'You do not have access to this quiz.'})
+  })
+})
+
+describe('Quiz not in trash', () => {
+  test('quiz 2 not in trash', () => {
+    expect(requestAdminQuizRestore(token1.body, quiz2.body.quizId).body).toStrictEqual({ error: 'Quiz not in trash.'})
+  })
+})
+
+describe('Invalid token', () => {
+  test('Invalid token created from invalid email', () => {
+    const invalidToken1 = requestAdminAuthRegister('', 'happy123', 'tommy', 'bommy');
+    expect(requestAdminQuizRestore(invalidToken1.body, quiz1.body.quizId).body).toStrictEqual({ error: 'Invalid token structure' })
+  })
+})
+
+describe('Invalid session', () => {
+  test('Test 1 invalid authUserId', () => {
+    const tokenInvalid = {
+      token: '-1'
+    }
+    expect(requestAdminQuizInfo(tokenInvalid, quiz1.body.quizId).body).toStrictEqual({ error: 'Not a valid session' })
+  })
+})
