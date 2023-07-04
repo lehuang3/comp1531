cf233236333506205e962e66505604d8c5854c11
Le <z5313327@ad.unsw.edu.au>
2023-07-04 13:42:10 +1000

    Created new branch for adminQuizRestore

---
+++ src/adminQuizRestore.test.ts
@@ -0,0 +1,3 @@
+import { adminQuizNameUpdate, adminQuizCreate } from './quiz.js'
+import { adminAuthLogin, adminAuthRegister } from './auth.js'
+import { requestClear, requestGetAdminUserDetails, requestAdminAuthRegister, requestAdminQuizNameUpdate, requestAdminQuizCreate} from './other'
