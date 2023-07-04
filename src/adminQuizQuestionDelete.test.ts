8e7be505f2f98c1c1686f7eeea7ddb140d6a08d2
Le <z5313327@ad.unsw.edu.au>
2023-07-04 22:30:37 +1000

    Made new branch for adminquizquestiondelete

---
+++ src/adminQuizQuestionDelete.test.ts
@@ -0,0 +1,51 @@
+import { adminQuizNameUpdate, adminQuizCreate } from './quiz.js'
+import { adminAuthLogin, adminAuthRegister } from './auth.js'
+import { requestClear, requestGetAdminUserDetails, requestAdminAuthRegister, requestAdminQuizCreate, requestAdminQuizInfo} from './other'
+
+beforeEach(() => {
+  requestClear();
+})
+
+describe('Passing cases', () => {
+
+});
+
+describe('Invalid quizId', () => {
+
+});
+
+describe('Quiz not owned', () => {
+
+});
+
+describe('Invalid questionId', () => {
+
+});
+
+beforeEach(() => {
+  requestClear();
+})
+
+describe('Passing cases', () => {
+
+});
+
+describe('Invalid quizId', () => {
+
+});
+
+describe('Quiz not owned', () => {
+
+});
+
+describe('All sessions not in END state', () => {
+
+});
+
+describe('Invalid session', () => {
+
+});
+
+describe('Invalid token', () => {
+
+});
