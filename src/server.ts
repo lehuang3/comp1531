f8837cae4e80fce6a07f634118724b54a8bcaf6d
Minh Le <z5422362@ad.unsw.edu.au>
2023-07-23 14:35:36 +1000

    fix: modify adminQuizTrashEmpty

--- src/server.ts
+++ src/server.ts
@@ -390,11 +390,7 @@ app.put('/v1/admin/quiz/:quizId/question/:questionId', (req: Request, res: Respo
 });
 
 app.delete('/v1/admin/quiz/trash/empty', (req: Request, res: Response) => {
-  let quizIdArr = req.query.quizIdArr as any[];
-  // if the array passed in is empty (no quizzes were chosen)
-  if (quizIdArr !== undefined) {
-    quizIdArr = quizIdArr.map(quizId => parseInt(quizId));
-  }
+  const quizIdArr = JSON.parse(req.query.quizIdArr as string);
   const token = req.query.token as string;
   const response = adminQuizTrashEmpty(token, quizIdArr);
   if ('error' in response) {
