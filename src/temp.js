3c010c7f66a7cd2dc36bfea51bfa5ee6c8539e35
Le <z5313327@ad.unsw.edu.au>
2023-07-25 21:10:17 +1000

    Deleted temp file'

--- src/temp.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-// URL of the image you want to download
-const imageUrl = 'https://www.verywellmind.com/thmb/AE-oEQeEe1a0kuIV4CqyBh8N14E=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/aphantasia-overview-4178710-final-e4d1ddfd5bad4f449598a18d18c73daf.png';
-const outputFileName = 'imageStore.json';
-
